e1041f817d7bcbb999d67ff2074f922c2ddd84c8
Christopher Martin <martincdev@gmail.com>
2018-08-01 22:51:14 -0500

    add key value to Card component map

--- src/CardList.js
+++ src/CardList.js
@@ -3,15 +3,21 @@ import Card from './Card';
 
 const CardList = ({robots}) => {
 
-  const cardArray = robots.map(user => {
-    return <Card id={user.id} name={user.name} email={user.email} />
-  });
-
   return (
     <div>
-      {cardArray}
+      {
+        robots.map(user => {
+          return (
+            <Card
+              key={user.id}
+              id={user.id}
+              name={user.name}
+              email={user.email}
+            />
+           );
+         })
+      }
     </div>
-
   )
 };
 
