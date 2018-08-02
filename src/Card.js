1add639215e791f6d3159522b4e8750ad265e65f
Christopher Martin <martincdev@gmail.com>
2018-08-01 19:55:00 -0500

    add robots.js, pass and destructure props

--- src/Card.js
+++ src/Card.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 
 
-class Card extends Component {
+class Card extends Component  {
+
   render() {
+    const { name, email, id } = this.props;
+
     return (
-      <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-        <img src='https://robohash.org/test?200x200' alt='_robots'/>
+      <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+        <img src={`https://robohash.org/${id}?200x200' alt='_robots`}/>
         <div>
-          <h2>Jane Doe</h2>
-          <p>jane.doe@gmail.com</p>
+          <h2>{name}</h2>
+          <p>{email}</p>
         </div>
       </div>
     );
