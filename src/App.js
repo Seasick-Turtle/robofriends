1119c8a668335c1a7e0dc3be0dd00077091ec387
Christopher Martin <martincdev@gmail.com>
2018-08-06 19:27:15 -0500

    add fetch in componentdidmount to fetch users and update state

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import {robots} from './robots';
 import './App.css';
 
-class App extends Component {
+export default class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: ''
-    }
+    };
+
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({robots: users}));
+
   }
 
   onSearchChange = (event) => {
@@ -35,5 +42,3 @@ class App extends Component {
   }
 
 }
-
-export default App;
