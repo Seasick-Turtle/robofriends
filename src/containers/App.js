992246d6da7cdfbda4b5e6d62fe6aa92a05732d7
Christopher Martin <martincdev@gmail.com>
2018-09-15 19:49:51 -0500

    add connect from redux, correct some typos

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import CardList from '../components/CardList';
 import Scroll from '../components/Scroll';
 import SearchBox from '../components/SearchBox';
 import ErrorBoundary from '../components/ErrorBoundary';
 import './App.css';
 
-export default class App extends Component {
+import { setSearchField } from '../actions';
+
+const mapStateToProps = state => {
+  return {
+    searchField: state.searchField
+  }
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+  }
+
+};
+
+class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      robots: [],
-      searchField: ''
+      robots: []
     };
 
   }
@@ -22,13 +37,9 @@ export default class App extends Component {
       .then(users => this.setState({robots: users}));
   }
 
-  onSearchChange = (event) => {
-    this.setState({ searchField: event.target.value });
-
-  };
-
   render() {
-    const { robots, searchField } = this.state;
+    const { robots} = this.state;
+    const { searchField, onSearchChange } = this.props;
     const filteredRobots = robots.filter(robot => {
       return robot.name.toLowerCase().includes(searchField.toLowerCase())
     });
@@ -39,7 +50,7 @@ export default class App extends Component {
 
         <div className='tc'>
           <h1 className='f1'>RoboFriends</h1>
-          <SearchBox searchChange={this.onSearchChange}/>
+          <SearchBox searchChange={onSearchChange}/>
           <Scroll>
             <ErrorBoundary>
               <CardList robots={filteredRobots}/>
@@ -51,3 +62,5 @@ export default class App extends Component {
   }
 
 }
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
