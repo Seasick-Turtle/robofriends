bfc2c4e8b7fef5d00cf60b251a928dfb8139d081
Christopher Martin <martincdev@gmail.com>
2018-09-16 17:36:36 -0500

    add redux logger to log actions

--- src/actions.js
+++ src/actions.js
@@ -1,9 +1,6 @@
 import { CHANGE_SEARCHFIELD } from './constants';
 
-export const setSearchField = (text) => {
-  console.log(text);
-  return {
-    type: CHANGE_SEARCHFIELD,
-    payload: text
-  }
-};
+export const setSearchField = (text) => ({
+  type: CHANGE_SEARCHFIELD,
+  payload: text
+});
